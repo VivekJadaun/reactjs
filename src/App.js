705d090a3859a15413c21bee5cca497b9c69807d
Vivek Jadaun <vivekjadaun08@gmail.com>
2021-01-05 17:36:26 +0530

    intermediate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Tab from './components/Tab';
+import SubTab from './components/SubTab';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>a
+        
+        <Tab name="introduction"></Tab>
+        <Tab name="the instance"></Tab>
+        <Tab name="list rendering"></Tab>
+                
+      </header>
     </div>
   );
 }
---
+++ src/components/SubTab.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+class SubTab extends Component {
+
+}
+
+export default SubTab
